0151f8ba30b3f76e4b85504cd878844f7bcf6c6f
Josh Leeb-du Toit <mail@joshleeb.com>
2019-06-16 12:07:48 +1000

    Add primitives for u24 and u32

--- message/src/codec.rs
+++ message/src/codec.rs
@@ -1,4 +1,5 @@
-use core::{mem, u16, u8};
+use crate::primitive::u24;
+use core::{u16, u32, u8};
 use decoder::Decoder;
 use encoder::Encoder;
 
@@ -39,30 +40,40 @@ pub enum HeaderSize {
     U8,
     /// 2 byte header.
     U16,
+    /// 3 byte header.
+    U24,
+    /// 4 byte header.
+    U32,
 }
 
 impl HeaderSize {
     pub fn size(&self) -> usize {
         match self {
-            HeaderSize::U8 => mem::size_of::<u8>(),
-            HeaderSize::U16 => mem::size_of::<u16>(),
             HeaderSize::Zero => 0,
+            HeaderSize::U8 => 1,
+            HeaderSize::U16 => 2,
+            HeaderSize::U24 => 3,
+            HeaderSize::U32 => 4,
         }
     }
 
     fn encode_len<'a>(&self, len: usize, enc: &mut Encoder<'a>) {
         match self {
+            HeaderSize::Zero => {}
             HeaderSize::U8 => HeaderSize::as_u8(len).encode(enc),
             HeaderSize::U16 => HeaderSize::as_u16(len).encode(enc),
-            HeaderSize::Zero => {}
+            HeaderSize::U24 => HeaderSize::as_u24(len).encode(enc),
+            HeaderSize::U32 => HeaderSize::as_u32(len).encode(enc),
         }
     }
 
     fn decode_len<'a>(&self, dec: &mut Decoder<'a>) -> Option<usize> {
         match self {
+            HeaderSize::Zero => None,
             HeaderSize::U8 => u8::decode(dec).map(usize::from),
             HeaderSize::U16 => u16::decode(dec).map(usize::from),
-            HeaderSize::Zero => None,
+            HeaderSize::U24 => u24::decode(dec).map(u24::as_u32).map(|x| x as usize),
+            HeaderSize::U32 => u32::decode(dec).map(|x| x as usize),
         }
     }
 
@@ -74,5 +85,16 @@ impl HeaderSize {
     fn as_u16(data: usize) -> u16 {
         debug_assert!(data <= usize::from(u16::MAX));
         data as u16
+    }
+
+    fn as_u24(data: usize) -> u24 {
+        debug_assert!(data <= usize::pow(2, 24) - 1);
+        u24::from(data as u32)
+    }
+
+    // TODO: Fix as_u32 which may panic on 16 or 32 bit devices?
+    fn as_u32(data: usize) -> u32 {
+        debug_assert!(data <= u32::MAX as usize);
+        data as u32
     }
 }
--- message/src/primitive.rs
+++ message/src/primitive.rs
@@ -1,5 +1,5 @@
 use crate::codec::{decoder::Decoder, encoder::Encoder, Codec, CodecSized, HeaderSize};
-use core::{mem, u16, u8};
+use core::{u16, u32, u8};
 
 impl<'a> Codec<'a> for u8 {
     fn encode(&self, enc: &mut Encoder<'a>) {
@@ -15,14 +15,13 @@ impl<'a> CodecSized<'a> for u8 {
     const HEADER_SIZE: HeaderSize = HeaderSize::U8;
 
     fn data_size(&self) -> usize {
-        mem::size_of::<Self>()
+        1
     }
 }
 
 impl<'a> Codec<'a> for u16 {
     fn encode(&self, enc: &mut Encoder<'a>) {
-        let buf = [(*self >> 8) as u8, *self as u8];
-        enc.append(buf);
+        enc.append([(*self >> 8) as u8, *self as u8]);
     }
 
     fn decode(dec: &mut Decoder<'a>) -> Option<Self> {
@@ -35,6 +34,69 @@ impl<'a> CodecSized<'a> for u16 {
     const HEADER_SIZE: HeaderSize = HeaderSize::U16;
 
     fn data_size(&self) -> usize {
-        mem::size_of::<Self>()
+        2
+    }
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+pub struct u24(u32);
+
+impl u24 {
+    pub fn as_u32(self) -> u32 {
+        self.0
+    }
+}
+
+impl<'a> Codec<'a> for u24 {
+    fn encode(&self, enc: &mut Encoder<'a>) {
+        enc.append([(self.0 >> 16) as u8, (self.0 >> 8) as u8, self.0 as u8]);
+    }
+
+    fn decode(dec: &mut Decoder<'a>) -> Option<Self> {
+        dec.take(u32::data_size(&0))
+            .map(|b| Self((u32::from(b[0]) << 16) | (u32::from(b[1]) << 8) | u32::from(b[2])))
+    }
+}
+
+impl<'a> CodecSized<'a> for u24 {
+    const HEADER_SIZE: HeaderSize = HeaderSize::U24;
+
+    fn data_size(&self) -> usize {
+        3
+    }
+}
+
+impl From<u32> for u24 {
+    fn from(data: u32) -> Self {
+        Self(data)
+    }
+}
+
+impl<'a> Codec<'a> for u32 {
+    fn encode(&self, enc: &mut Encoder<'a>) {
+        enc.append([
+            (*self >> 24) as u8,
+            (*self >> 16) as u8,
+            (*self >> 8) as u8,
+            *self as u8,
+        ]);
+    }
+
+    fn decode(dec: &mut Decoder<'a>) -> Option<Self> {
+        dec.take(u32::data_size(&0)).map(|b| {
+            (u32::from(b[0]) << 24)
+                | (u32::from(b[1]) << 16)
+                | (u32::from(b[2]) << 8)
+                | u32::from(b[3])
+        })
+    }
+}
+
+impl<'a> CodecSized<'a> for u32 {
+    const HEADER_SIZE: HeaderSize = HeaderSize::U32;
+
+    fn data_size(&self) -> usize {
+        3
     }
 }
