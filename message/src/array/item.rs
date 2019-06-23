cf4a55db53300fde96622aec77db6db35cb1af56
Josh Leeb-du Toit <mail@joshleeb.com>
2019-06-23 13:33:28 +1000

    Add equality tests for array Item

--- message/src/array/item.rs
+++ message/src/array/item.rs
@@ -27,9 +27,51 @@ impl<'a, T: PartialEq + Codec<'a>> PartialEq for Item<'a, T> {
         self.as_ref() == other.as_ref()
     }
 }
+impl<'a, T: Eq + Codec<'a>> Eq for Item<'a, T> {}
 
 impl<'a, T: PartialEq + Codec<'a>> PartialEq<T> for Item<'a, T> {
     fn eq(&self, other: &T) -> bool {
         self.as_ref() == other
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn eq_borrowed() {
+        let value: (u32, u32) = (1, 1);
+        assert_eq!(Item::Borrowed(&value.0), Item::Borrowed(&value.1));
+    }
+
+    #[test]
+    fn ne_borrowed() {
+        let value: (u32, u32) = (1, 2);
+        assert_ne!(Item::Borrowed(&value.0), Item::Borrowed(&value.1));
+    }
+
+    #[test]
+    fn eq_owned() {
+        let value: (u32, u32) = (1, 1);
+        assert_eq!(Item::Owned(value.0), Item::Owned(value.1));
+    }
+
+    #[test]
+    fn ne_owned() {
+        let value: (u32, u32) = (1, 2);
+        assert_ne!(Item::Owned(value.0), Item::Owned(value.1));
+    }
+
+    #[test]
+    fn eq_mixed() {
+        let value: (u32, u32) = (1, 1);
+        assert_eq!(Item::Owned(value.0), Item::Borrowed(&value.1));
+    }
+
+    #[test]
+    fn ne_mixed() {
+        let value: (u32, u32) = (1, 2);
+        assert_ne!(Item::Owned(value.0), Item::Borrowed(&value.1));
     }
 }
